1b3cbd227f6300d07604ff61fb331be64fdaa645
cwjohnson7 <cliffj1230@gmail.com>
2023-04-09 23:14:17 -0400

    referencing state in the list items

--- src/components/bracket.js
+++ src/components/bracket.js
@@ -29,35 +29,35 @@ const BracketView = () => {
         <div className="left-hand-bracket">
     <ul className="matchup">
       <li className="round1A" onClick={handleSelectedWinnerClick}><span className="seed">1</span> {contestants.brackets[0].round1A[0]}</li>
-      <li className="round1A"><span className="seed">8</span> Album 2</li>
+      <li className="round1A"><span className="seed">8</span> {contestants.brackets[0].round1A[1]}</li>
     </ul>
     <ul className="matchup">
-      <li className="round1B"><span className="seed">4</span> Album 3</li>
-      <li className="round1B"><span className="seed">5</span> Album 4</li>
+      <li className="round1B"><span className="seed">4</span> {contestants.brackets[1].round1B[0]}</li>
+      <li className="round1B"><span className="seed">5</span> {contestants.brackets[1].round1B[1]}</li>
     </ul>
         </div>
     <div className="right-hand-bracket">
     <ul className="matchup">
-      <li className="round1C"><span className="seed">2</span> Album 5</li>
-      <li className="round1C"><span className="seed">7</span> Album 6</li>
+      <li className="round1C"><span className="seed">2</span> {contestants.brackets[2].round1C[0]}</li>
+      <li className="round1C"><span className="seed">7</span> {contestants.brackets[2].round1C[1]}</li>
     </ul>
     <ul className="matchup">
-      <li className="round1D"><span className="seed">3</span> Album 7</li>
-      <li className="round1D"><span className="seed">6</span> Album 8</li>
+      <li className="round1D"><span className="seed">3</span> {contestants.brackets[3].round1D[0]}</li>
+      <li className="round1D"><span className="seed">6</span> {contestants.brackets[3].round1D[1]}</li>
     </ul>
     </div>
     </div>
     <div className="col-1-8">
       <div className="round-two-left">
         <ul className="matchup">
-          <li className="round2A"><span className="seed">1</span> Winner 1</li>
-          <li className="round2A"><span className="seed">4</span> Winner 2</li>
+          <li className="round2A"><span className="seed">1</span> Winner 1: {contestants.brackets[4].round2A[0]}</li>
+          <li className="round2A"><span className="seed">4</span> Winner 2: {contestants.brackets[4].round2A[1]}</li>
         </ul>
     </div>
       <div className="round-two-right">
       <ul className="matchup round-two-bottom">
-        <li className="round2B"><span className="seed">2</span> Winner 3</li>
-        <li className="round2B"><span className="seed">3</span> Winner 4</li>
+        <li className="round2B"><span className="seed">2</span> Winner 3: {contestants.brackets[5].round2B[0]}</li>
+        <li className="round2B"><span className="seed">3</span> Winner 4: {contestants.brackets[5].round2B[1]}</li>
       </ul>
     </div>
   </div>
